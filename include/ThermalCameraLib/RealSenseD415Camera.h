44a77e73dda57d3656775cad2ce1d93e68c8e09f
Abian Hernandez <abianhernandez@outlook.es>
2019-10-03 16:29:18 +0100

    Intel Realsense working

--- include/ThermalCameraLib/RealSenseD415Camera.h
+++ include/ThermalCameraLib/RealSenseD415Camera.h
@@ -10,21 +10,33 @@ namespace mt4sd {
     {
         public:
             RealSenseD415Camera();
-            ~RealSenseD415Camera();
+            ~RealSenseD415Camera() override;
+
+            QImage * getDisplayFrame() override;
 
         public slots:
             bool connect() override;
             bool disconnect() override;
 
         private:
+            rs2::context ctx;
             rs2::pipeline pipe;
-            rs2::config cfg;
             rs2::pipeline_profile profile;
-            rs2_stream alignTo;
-            rs2::align align;
+            rs2::config cfg;
             rs2::colorizer colorMap;
 
+            rs2_stream alignTo;
+            rs2::align align;
+
+            rs2::decimation_filter dec;
+            rs2::spatial_filter spat;
+            rs2::temporal_filter temp;
+            rs2::disparity_transform depth2disparity;
+            rs2::disparity_transform disparity2depth;
+
             rs2_stream findStreamToAlign(const std::vector<rs2::stream_profile> &streams);
+            void configureCamera();
+            void getDisplayFrame(const uint16_t *frame, int size, unsigned char *display);
     };
 }
 
--- src/RealSenseD415Camera.cpp
+++ src/RealSenseD415Camera.cpp
@@ -2,13 +2,100 @@
 
 #include <QDebug>
 
+// Importante, usar el ejemplo rs-measure como referencia! :D
+
 mt4sd::RealSenseD415Camera::RealSenseD415Camera() :
-    Camera(), pipe(), cfg(), profile(), alignTo(), align(alignTo), colorMap()
-{}
+    Camera(), ctx(), pipe(ctx), profile(), cfg(), colorMap(), alignTo(RS2_STREAM_DEPTH), align(alignTo),
+    dec(), spat(), temp(), depth2disparity(), disparity2depth(false)
+{
+    configureCamera();
+    if (this->connect()){
+        frameWidth = 640;
+        frameHeight = 480;
+        //displayableFrame = new QImage(new uchar[getFrameSize()], getFrameWidth(), getFrameHeight(), QImage::Format::Format_Grayscale8);
+        displayableFrame = new QImage(new uchar[getFrameSize()*3], getFrameWidth(), getFrameHeight(), QImage::Format::Format_RGB888);
+    }
+}
 
 mt4sd::RealSenseD415Camera::~RealSenseD415Camera()
 {
     // Will be disconnected automatically before being destroyed
+}
+
+QImage * mt4sd::RealSenseD415Camera::getDisplayFrame()
+{
+    //If it is too slow, you can increase the following parameter to decimate depth more (reducing quality)
+    dec.set_option(RS2_OPTION_FILTER_MAGNITUDE, 1);
+
+    //rs2::frameset frames = pipe.wait_for_frames();
+    rs2::frameset data;
+    if(pipe.poll_for_frames(&data))
+    {
+        data = data.apply_filter(align);
+
+        // Decimation will reduce the resultion of the depth image,
+        // closing small holes and speeding-up the algorithm
+        data = data.apply_filter(dec);
+
+        // To make sure far-away objects are filtered proportionally
+        // we try to switch to disparity domain
+        data = data.apply_filter(depth2disparity);
+
+        // Apply spatial filtering
+        data = data.apply_filter(spat);
+
+        // Apply temporal filtering
+        data = data.apply_filter(temp);
+
+        // If we are in disparity domain, switch back to depth
+        data = data.apply_filter(disparity2depth);
+
+        //// Apply color map for visualization of depth
+        data = data.apply_filter(colorMap);
+
+        auto depth = data.get_depth_frame();
+        auto color = data.get_color_frame();
+
+//        qDebug() << depth.get_data_size();
+        //const uint16_t *test2 = static_cast<const uint16_t *>(depth.get_data());
+        const uchar *color_test2 = static_cast<const uchar *>(color.get_data());
+
+        for(size_t pixel=0; pixel < this->getFrameSize(); ++pixel)
+        {
+            for (ssize_t channel=2; channel >= 0; --channel)
+            {
+                displayableFrame->bits()[pixel * 3 + 2-channel] = color_test2[pixel * 3 + channel];
+            }
+        }
+
+        //getDisplayFrame(test2, this->getFrameSize(), displayableFrame->bits());
+
+
+//        auto colorized_depth = data.first(RS2_STREAM_DEPTH, RS2_FORMAT_RGB8);
+
+//        colorized_depth.get_data();
+
+
+//        auto depth = data.get_depth_frame();
+//        qDebug() << depth.get_data_size();
+
+//        rs2::frame  depth_texture;
+//        depth_texture = colorMap.colorize(depth);
+
+//        depth_texture.get_data();
+//        const uint16_t *test2 = static_cast<const uint16_t *>(depth.get_data());
+//        for (size_t i=0; i < 10; ++i){
+//            //qDebug() << static_cast<uint16_t *>(test[0]);
+//            //qDebug() << static_cast<char *[10]>(test)[0][0];
+//            //qDebug() << QVariant::fromValue(test);
+//            qDebug() << test2[i];
+//        }
+//        //data = reinterpret_cast<uchar>(depth.get_data());
+//        qDebug() << "Parate aquí";
+//        //TODO
+    }
+
+    return displayableFrame;
 }
 
 bool mt4sd::RealSenseD415Camera::connect()
@@ -17,6 +104,12 @@ bool mt4sd::RealSenseD415Camera::connect()
         profile = pipe.start(cfg);
         //alignTo = findStreamToAlign(profile.get_streams());
         align   = rs2::align(alignTo);
+
+        //Set the device to High Accuracy preset
+        rs2::depth_sensor sensor = profile.get_device().first<rs2::depth_sensor>();
+        if (sensor && sensor.is<rs2::depth_stereo_sensor>())
+            sensor.set_option(RS2_OPTION_VISUAL_PRESET, RS2_RS400_VISUAL_PRESET_HAND);
+
         return true;
 
     } catch (const std::runtime_error& error) {
@@ -45,4 +138,31 @@ bool mt4sd::RealSenseD415Camera::disconnect()
     }
 
     return false;
+}
+
+void mt4sd::RealSenseD415Camera::configureCamera()
+{
+    cfg = rs2::config();
+    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
+    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30); //16-bit depth values
+}
+
+void mt4sd::RealSenseD415Camera::getDisplayFrame(const uint16_t *frame, int size, unsigned char *display)
+{
+    unsigned short min = 65535;
+    unsigned short max = 0;
+    //get min/max values -> AGC
+    for(int i=0; i<size; i++)
+    {
+        min = frame[i] < min ? frame[i] : min;
+        max = frame[i] > max ? frame[i] : max;
+    }
+    //cout << "AGC: min = " << min << " - max = " << max << endl;
+    //now scale ouput data
+    //TBD: flip upside down!
+    double invDelta = 1.0 / (static_cast<double>(max-min));
+    for(int i=0;i<size;i++)
+    {
+        display[i] = static_cast<unsigned char>((255.0 * (frame[i] - min)) * invDelta);
+    }
 }
