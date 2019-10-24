d25a8cd395e64c72db66ff5fd7f9b8a1b6c44abd
Abian Hernandez <abianhernandez@outlook.es>
2019-10-24 11:40:19 +0100

    ...

--- cmake/ThermalCameraLibConfig.cmake
+++ cmake/ThermalCameraLibConfig.cmake
@@ -6,10 +6,10 @@ if (EXISTS ${SELF_DIR}/ThermalCameraLib.cmake)
 
     get_target_property(THERMALCAMERALIB_INCLUDE_DIR ThermalCameraLib INTERFACE_INCLUDE_DIRECTORIES)
     get_target_property(THERMALCAMERALIB_LIB_DIR ThermalCameraLib IMPORTED_LOCATION_DEBUG) #A cambiar
-    message(STATUS "Found Colormap-shaders:")
+    message(STATUS "Found ThermalCameraLib:")
     message(STATUS " - Includes: ${THERMALCAMERALIB_INCLUDE_DIR}")
     message(STATUS " - Library: ${THERMALCAMERALIB_INCLUDE_DIR}")
-
+    
 else()
     message(STATUS "Not Found ThermalCameraLib")
 endif()
